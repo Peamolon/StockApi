3aa58b734733768f06c25121af4d5f00b908e1aa
Sergio Peña <sergioeduardo2000@gmail.com>
2024-12-28 14:55:48 -0500

    Add method to use fake data

--- src/hooks/useWebSocket.js
+++ src/hooks/useWebSocket.js
@@ -6,35 +6,7 @@ const useWebSocket = (symbols, useLiveData = false) => {
   const socket = useRef(null);
 
   useEffect(() => {
-    if (useLiveData) {
-      socket.current = new WebSocket(`wss://ws.finnhub.io?token=${FINNUB_API_KEY}`);
-
-      socket.current.onopen = () => {
-        console.log("WebSocket connection established");
-        if (socket.current.readyState === WebSocket.OPEN) {
-          symbols.forEach(symbol => {
-            socket.current.send(JSON.stringify({ type: 'subscribe', symbol }));
-          });
-        }
-      };
-
-      socket.current.onmessage = (event) => {
-        console.log('Message from server: ', event.data);
-        const message = JSON.parse(event.data);
-        if (message.type === 'trade') {
-          handleNewPrice(message.data[0].s, new Date(message.data[0].t), message.data[0].p);
-        }
-      };
-
-      return () => {
-        if (socket.current.readyState === WebSocket.OPEN) {
-          symbols.forEach(symbol => {
-            socket.current.send(JSON.stringify({ type: 'unsubscribe', symbol }));
-          });
-        }
-        socket.current.close();
-      };
-    } else {
+    if (!useLiveData) {
       const intervalId = setInterval(() => {
         symbols.forEach(symbol => {
           const simulatedPrice = generateRandomPrice();
@@ -42,21 +14,49 @@ const useWebSocket = (symbols, useLiveData = false) => {
           handleNewPrice(symbol, now, simulatedPrice);
         });
       }, 1000);
-
       return () => clearInterval(intervalId);
     }
+
+    if (socket.current && socket.current.readyState === WebSocket.OPEN) {
+      socket.current.close();
+    }
+
+    socket.current = new WebSocket(`wss://ws.finnhub.io?token=${FINNUB_API_KEY}`);
+    socket.current.onopen = () => {
+      console.log("WebSocket connection established");
+      symbols.forEach(symbol => {
+        socket.current.send(JSON.stringify({ type: 'subscribe', symbol }));
+      });
+    };
+    socket.current.onmessage = (event) => {
+      console.log('Message from server: ', event.data);
+      const message = JSON.parse(event.data);
+      if (message.type === 'trade') {
+        handleNewPrice(message.data[0].s, new Date(message.data[0].t), message.data[0].p);
+      }
+    };
+
+    return () => {
+      if (socket.current.readyState === WebSocket.OPEN) {
+        symbols.forEach(symbol => {
+          socket.current.send(JSON.stringify({ type: 'unsubscribe', symbol }));
+        });
+      }
+      socket.current.close();
+    };
   }, [symbols, useLiveData]);
 
   const handleNewPrice = (symbol, date, price) => {
     setStocks(prevStocks => {
       const index = prevStocks.findIndex(s => s.symbol === symbol);
       const newPrice = { date: date.toISOString(), price };
+      const updatedStocks = [...prevStocks];
       if (index !== -1) {
-        const newStocks = [...prevStocks];
-        newStocks[index].prices.push(newPrice);
-        return newStocks;
+        updatedStocks[index].prices.push(newPrice);
+      } else {
+        updatedStocks.push({ symbol, prices: [newPrice] });
       }
-      return prevStocks;
+      return updatedStocks;
     });
   };
 
