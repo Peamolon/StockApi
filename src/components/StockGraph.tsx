aa5ab3fab37b5368eba26c6ac6f8e35c13485860
Sergio Peña <sergioeduardo2000@gmail.com>
2024-12-29 12:48:11 -0500

    StockGraph: Refactor graphics to rewrite in real time

--- src/components/StockGraph.tsx
+++ src/components/StockGraph.tsx
@@ -1,42 +1,29 @@
-import React from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-interface StockData {
-  symbol: string;
-  prices: { date: string; price: number }[];
-}
+import React from 'react';
+import { StockInfo } from '../models/StockInfo';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 interface StockGraphProps {
-  stocks: StockData[];
+  stocks: StockInfo[];
 }
 
 const StockGraph: React.FC<StockGraphProps> = ({ stocks }) => {
-  const data = stocks.flatMap(stock => stock.prices.map(price => ({
+  const data = stocks.flatMap(stock => 
+    stock.prices.map(price => ({
+      name: stock.symbol,
       date: price.date,
-      [stock.symbol]: price.price
-  })));
+      price: price.price
+    }))
+  );
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
-      <LineChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-        }}
-      >
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="date" />
         <YAxis />
         <Tooltip />
-        <Legend />
-        {stocks.map(stock => (
-            <Line key={stock.symbol} type="monotone" dataKey={stock.symbol} stroke="#8884d8" activeDot={{ r: 8 }} />
-        ))}
+        <Line type="monotone" dataKey="price" stroke="#8884d8" activeDot={{ r: 8 }} />
       </LineChart>
     </ResponsiveContainer>
   );
